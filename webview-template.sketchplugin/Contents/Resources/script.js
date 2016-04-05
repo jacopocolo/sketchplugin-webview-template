5f8c6f0a0337f2e439bbb8cfecc5fe35aa51c7f0
Jacopo Colo <JACO@Designits-MacBook-Pro-8.local>
2016-04-05 19:41:53 +0200

    Added a simple way to send data from Sketch to webview and from webview to Sketch

---
+++ webview-template.sketchplugin/Contents/Resources/script.js
@@ -0,0 +1,1 @@
+document.getElementById('myInput').value = '4'
--- webview-template.sketchplugin/Contents/Sketch/script.js
+++ webview-template.sketchplugin/Contents/Sketch/script.js
@@ -1,18 +1,41 @@
-var pluginName = "webview-template"
+var fileName = "webview-template.sketchplugin"
 
 function onRun(context) {
+	//You do whatever you want with with context, I expose these
+	ctx = context;
+  doc = context.document;
+  selection = context.selection;
+  page = [doc currentPage];
+  view = [doc currentView];
+  artboards = [[doc currentPage] artboards];
 
 	var defaults = [NSUserDefaults standardUserDefaults], default_values = [NSMutableDictionary dictionary];
 
 	// Let’s create a window for the ui
 	var window = [[NSWindow alloc] init]
-	var windowTitle = pluginName
+	var windowTitle = "Webview template"
 	[window setTitle:windowTitle]
   //Let’s create a 500x420 pixels window
 	[window setFrame:NSMakeRect(0, 0, 500, 420) display:false]
 
+	//Do you want to pass something from Sketch to the WebView?
+	//I tried several different options and I found this to be the easiest:
+	//you save a file with the function that you want to run and webview loads it.
+	//There should be a function in cocoa to check if the webview is fully loaded
+	//but I never managed to get it to work in cocoascript.
+	var count = [artboards count]; // example value imported from Sketch
+	function saveScript(code) {
+	    var string = [NSString stringWithFormat: "%@", code],
+	      filePath = "/Users/" + NSUserName() + "/Library/Application Support/com.bohemiancoding.sketch3/Plugins/"+fileName+"/Contents/Resources/script.js";
+			[string writeToFile: filePath atomically: true
+	      encoding: NSUTF8StringEncoding error: nil];
+	}
+	//The javascript you want to run in the webview.
+	//In this case we set the value of myInout to 10
+	saveScript("document.getElementById('myInput').value = '"+count+"'");
+
   //Let’s set up the path of the html page we want to load
-  var filePath = "/Users/" + NSUserName() + "/Library/Application Support/com.bohemiancoding.sketch3/Plugins/"+pluginName+".sketchplugin/Contents/Resources/ui.html";
+  var filePath = "/Users/" + NSUserName() + "/Library/Application Support/com.bohemiancoding.sketch3/Plugins/"+fileName+"/Contents/Resources/ui.html";
 
   //Let’s set up a frame for the webview, a little smaller than the window so we have room for controls
   var frame = NSMakeRect(0,60,500,340);
